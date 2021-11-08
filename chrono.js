64ebca9af2d7d74c85b3249237026f9afa64a476
Damien Lopvet <etwincorp@gmail.com>
2021-11-08 07:46:30 -0300

    Change methode to be able to run the stopwatch while tab is inactive
    setting litle css responsive

--- chrono.js
+++ chrono.js
@@ -1,30 +1,36 @@
-var x;
 var startstop = 0;
-
-function startStop() { /* Toggle StartStop */
+var x;
+function startStop() {
+  /* Toggle StartStop */
 
   startstop = startstop + 1;
 
   if (startstop === 1) {
     start();
     document.querySelector(".start_stop").innerHTML = "Stop";
-    document.querySelector(".start_stop").style.backgroundColor = "#EF6262"
+    document.querySelector(".start_stop").style.backgroundColor = "#EF6262";
   } else if (startstop === 2) {
     document.querySelector(".start_stop").innerHTML = "Start";
-    document.querySelector(".start_stop").style.backgroundColor = "#59b87e"
+    document.querySelector(".start_stop").style.backgroundColor = "#59b87e";
     startstop = 0;
     stop();
   }
-
 }
 
-
 function start() {
-  x = setInterval(timer, 10);
+  if (!x) {
+    startedAt = Date.now();
+    x = setInterval(timer, 10);
+  }else {
+    startedAt = Date.now()- milisec
+    x = setInterval(timer, 10);
+  }
 } /* Start */
 
 function stop() {
-  clearInterval(x);
+  if (x) {
+    clearInterval(x);
+  }
 } /* Stop */
 
 var milisec = 0;
@@ -41,45 +47,24 @@ var hourOut = 0;
 
 /* Output variable End */
 
-
 function timer() {
   /* Main Timer */
 
-
-  miliSecOut = checkTime(milisec);
+  milisec = Date.now() - startedAt;
+   second = Math.floor(milisec / 1000);
+  hour = Math.floor(second / 3600);
+  min = Math.floor((second - hour * 3600) / 60);
+  sec = second - hour * 3600 - min * 60;
   secOut = checkTime(sec);
   minOut = checkTime(min);
   hourOut = checkTime(hour);
-
-  milisec = ++milisec;
-
-  if (milisec === 100) {
-    milisec = 0;
-    sec = ++sec;
-  }
-
-  if (sec == 60) {
-    min = ++min;
-    sec = 0;
-  }
-
-  if (min == 60) {
-    min = 0;
-    hour = ++hour;
-
-  }
-
-
-
   document.getElementById("minutes").innerHTML = minOut;
   document.getElementById("hours").innerHTML = hourOut;
-  document.getElementById("secondes").innerHTML = secOut
-document.querySelector('#setHours').innerHTML = hourOut;
+  document.getElementById("secondes").innerHTML = secOut;
+  document.querySelector("#setHours").innerHTML = hourOut;
 }
 
-
 /* Adds 0 when value is <10 */
-
 
 function checkTime(i) {
   if (i < 10) {
@@ -89,25 +74,38 @@ function checkTime(i) {
 }
 
 function reset() {
-
-
   /*Reset*/
 
   milisec = 0;
   sec = 0;
-  min = 0
+  min = 0;
   hour = 0;
-
+  document.getElementById("secondes").innerHTML = "00";
   document.getElementById("minutes").innerHTML = "00";
   document.getElementById("hours").innerHTML = "00";
-  document.getElementById("hours").innerHTML = "00"
-  
+
+  document.getElementById("hours").innerHTML = "00";
 }
 
-document.querySelector("#setHours").addEventListener("input", ()=>{
+document.querySelector("#setHours").addEventListener("change", () => {
+  newHour = document.querySelector("#setHours").value;
+  hour = startedAt - newHour * 3600000;
+  if(hour > Date.now()){
+    document.querySelector("#setHours").value = "";
+    return
+  }
+  startedAt = hour;
+  document.querySelector("#setHours").value = "";
+});
+document.querySelector("#setMinutes").addEventListener("change", () => {
+  newMinutes = document.querySelector("#setMinutes").value;
+  minute = startedAt - newMinutes * 60000;
   
-  newHour = document.querySelector("#setHours").value
-  if(newHour.toString().length < 3){
-  hour = newHour}
-  console.log(newHour)
-})
+  if (minute > Date.now()) {
+    
+    document.querySelector("#setMinutes").value = "";
+    return;
+  }
+  startedAt = minute;
+  document.querySelector("#setMinutes").value = "";
+});
